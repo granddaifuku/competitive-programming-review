851eaa827cce9a559be52cd3b53450f9133983c4
granddaifuku <daifukuformac@gmail.com>
2021-05-19 14:29:09 +0900

    moved to sqlx

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+test:
+	docker compose up -d --build db
+	DATABASE_URL="postgres://postgres:password@localhost:5432/test" cargo test
+
 down:
-	docker-compose down --rmi all
+	docker compose down --rmi all
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+mod config;
 mod db;
 
+use sqlx::postgres::PgPoolOptions;
+
 fn main() {
+    let config = config::Config::new();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&config.database_url);
+
     rocket::ignite().launch();
 }
