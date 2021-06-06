318a526a4eee996ebc9ba6509984c08d32e78eed
granddaifuku <daifukuformac@gmail.com>
2021-06-07 00:33:38 +0900

    create new struct for log in

--- src/users/model.rs
+++ src/users/model.rs
@@ -11,3 +11,11 @@ pub struct NewUser {
     #[validate(length(min = 1, max = 100), regex(path = "RE_ALP_NUM_SYM"))]
     pub password: String,
 }
+
+#[derive(Debug, Serialize, Deserialize, Validate, sqlx::FromRow, PartialEq)]
+pub struct LoginUser {
+    #[validate(length(min = 1, max = 100), regex(path = "RE_ALP_NUM_SYM"))]
+    pub user_name: String,
+    #[validate(length(min = 1, max = 100), regex(path = "RE_ALP_NUM_SYM"))]
+    pub password: String,
+}
