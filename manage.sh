5762361b28970ac024620b7f7aa88323103d9693
granddaifuku <daifukuformac@gmail.com>
2021-06-07 00:33:22 +0900

    add shell script to clear db table

--- manage.sh
+++ manage.sh
@@ -3,4 +3,6 @@ export PGPASSWORD=password
 
 if [ $1 = "enterdb" ]; then
 	psql -h 127.0.0.1 -p 5432 -U postgres test
+elif [ $1 = "cleartable" ]; then
+	psql -h 127.0.0.1 -p 5432 -f ./config/init.sql -U postgres -d test
 fi
